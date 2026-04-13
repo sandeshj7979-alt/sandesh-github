c7664a6431e9f98f237825d4fce3a032939bd25e
sandeshj7979-alt <sandeshj7979@gmail.com>
2026-04-13 20:02:00 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// MOUSE FOLLOW GLOW (WORKING FIX)
-const glow = document.createElement("div");
-glow.classList.add("mouse-glow");
-document.body.appendChild(glow);
-
-document.addEventListener("mousemove", (e) => {
-    glow.style.left = e.clientX + "px";
-    glow.style.top = e.clientY + "px";
-});
